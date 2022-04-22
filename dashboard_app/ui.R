1b2988731ede575f775001dcafb215fca5841bf9
mrcslx <100318856+mrcslx@users.noreply.github.com>
2022-04-22 19:47:32 +0100

    Trying tabs

--- dashboard_app/ui.R
+++ dashboard_app/ui.R
@@ -1,7 +1,26 @@
 ui <- dashboardPage(skin = "purple", 
-                    dashboardHeader(title = "Basic dashboard"),
-                    dashboardSidebar(),
+                    dashboardHeader(title = "Epic dashboard"),
+                    dashboardSidebar(
+                      
+                      sidebarMenu(
+                        menuItem("Winter Crisis!", tabName = "tab one", icon = icon("stats", lib = "glyphicon"), 
+                                 badgeLabel = "cool!", badgeColor = "green"),
+                        menuItem("Covid Map!", icon = icon("globe", lib = "glyphicon"), tabName = "tab two",
+                                 badgeLabel = "also cool!", badgeColor = "green")
+                      )
+                      
+                    ),
                     dashboardBody(
+                      
+                      tabItems(
+                        tabItem(tabName = "tab one",
+                                h2("Crisis tab content")
+                        ),
+                        
+                        tabItem(tabName = "tab two",
+                                h2("Map tab content")
+                        )
+                      ),
                       # Boxes need to be put in a row (or column)
                       fluidRow(
                         box(plotOutput("plot1", height = 250)),
