73e1f7a0a768d8e8d696d39083a7351c796ed117
TomKeeling <100126049+TomKeeling@users.noreply.github.com>
2022-04-25 14:47:27 +0100

    merged with Marcus successfully

--- dashboard_app/helpers.R
+++ dashboard_app/helpers.R
@@ -1,10 +1,5 @@
 
 
-library(tidyverse)
-library(sf)
-library(shiny)
-
-
 #   ----------------------GENERATING MAP DATA---------------------------------
 
 
@@ -44,3 +39,32 @@ boundary_esri <- st_read(
 
 beds_geom <- beds_clean %>% left_join(boundary_esri, by = "hb")
 
+
+admissions <- read_csv(here::here("raw_data/hospital_admissions_hb_simd_20220302.csv")) %>%
+  janitor::clean_names()
+hospitals <- read_csv(here::here("raw_data/current-hospital_flagged20211216.csv")) %>% 
+  janitor::clean_names()
+
+hospitals <- hospitals %>%
+  drop_na(x_coordinate)
+
+# hospitals <- st_as_sf(hospitals, coords = c("x_coordinate", "y_coordinate")) %>%
+#   st_set_crs(27700) %>%   #set coordinate system used
+#   st_transform(4326)     #transform coordinates to WGS84 coordinates
+
+# hospitals %>%
+#   leaflet() %>%
+#   addProviderTiles(providers$CartoDB.Voyager) %>%
+#   addCircleMarkers(data = hospitals$geometry,
+#                    color = "#fdae61",
+#                    fillOpacity = 0.5,
+#                    radius = 2)
+
+# Convert df to sf, 27700 is the EPSG code for the UK
+hospitals <- st_as_sf(hospitals,
+                      coords = c("x_coordinate", "y_coordinate"),
+                      crs = 27700)
+
+# Reproject to WGS84
+hospitals <- st_transform(hospitals , 4326)
+hospitals <- sf_to_df(hospitals, fill = TRUE)
