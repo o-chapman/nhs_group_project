6dae2643b1872dcd8fe15db50dffa64bfeefd1da
guild13 <63346560+guild13@users.noreply.github.com>
2022-04-27 08:37:58 +0100

    graphs and infoboxes
    all done except length of stay infoboxes

--- dashboard_app/server.R
+++ dashboard_app/server.R
@@ -1,19 +1,6 @@
 server <- function(input, output) {
-
- ## TAB 1
-
-  output$overview_beds_plot <- renderPlot({
-
-    beds_season_subset %>%
-      filter(year %in% input$year_input) %>%
-      ggplot() +
-      aes(x = date, y = avg_daily_beds_perc, label = str_c(round(avg_daily_beds_perc), "%")) +
-      geom_text(nudge_y = 1.2, alpha = 0.8) +
-      geom_col(fill = "turquoise", alpha = 0.8) +
-      coord_cartesian(ylim = c(60, 80)) +
-      labs(x = "") +
-      theme_minimal()
-  })
+  
+  ## TAB 1
   
   admissions_dt_react <- reactive({
     
@@ -22,51 +9,181 @@ server <- function(input, output) {
     
   })
   
+  ## INFO BOXES
+  
+  output$info_box_max <- renderInfoBox({
+    
+    
+  })
+  
+  ## PLOTS
+  
   output$overview_admissions_plot <- renderPlot({
-
+    
+    validate(
+      need(input$year_input, "Please tick a year above")
+    )
     admissions_dt_react() %>%
       ggplot() +
       aes(x = date, y = avg_admissions_by_week) +
-      geom_line(color = "steelblue") +
+      geom_line(color = "steelblue", size = 1.5) +
       winter_shading[1] +
       winter_shading[2] +
       scale_x_date(name = "",
-                   #limits = c(as.Date("2020-01-01", "%Y-%m-%d"), as.Date("2022-02-20", "%Y-%m-%d")),
-                   limits = c(min(admissions_dt_react()$date), max(admissions_dt_react()$date)),
+                   limits = c(min(admissions_dt_react()$date) - days(7), max(admissions_dt_react()$date) + days(7)),
                    date_breaks = "3 months",
                    date_minor_breaks = "1 month",
                    date_labels = "%b %y") +
-      labs(y = "Average Admissions(weekly)") +
-      theme_minimal()
+      coord_cartesian(clip = "off") +
+      labs(y = "Average Admissions(weekly)\n") +
+      theme_minimal()     
+    
   })
-
+  
+  output$overview_beds_plot <- renderPlot({
+    
+    validate(
+      need(input$year_input, "Please tick a year above")
+    )
+    beds_season_subset %>%
+      filter(year %in% input$year_input) %>%
+      ggplot() +
+      aes(x = date, y = avg_daily_beds_perc, label = str_c(round(avg_daily_beds_perc), "%")) +
+      geom_text(nudge_y = 1.2, alpha = 0.8) +
+      geom_col(fill = "turquoise", alpha = 0.8) +
+      coord_cartesian(ylim = c(60, 80)) +
+      labs(x = "",
+           y = "Daily available beds (%)\n") +
+      theme_minimal()      
+    
+  })
+  
   output$overview_los_plot <- renderPlot({
-
-    ggplotly(admissions_dt %>%
-               ggplot() +
-               aes(x = date, y = avg_admissions_by_week) +
-               geom_line(color = "steelblue") +
-               scale_x_date(name = "", limits = c(as.Date("2020-01-01", "%Y-%m-%d"), as.Date("2022-02-20", "%Y-%m-%d")), date_breaks = "3 months",
-                            date_minor_breaks = "1 month", date_labels = "%b %y") +
-               labs(y = "Average Admissions(weekly)") +
-               theme_minimal()
+    
+    validate(
+      need(input$year_input, "Please tick a year above")
     )
-
+    los_season_subset %>%
+      filter(year %in% input$year_input) %>%
+      ggplot() +
+      aes(x = date, y = avg_los_days, label = str_c(avg_los_days, " days")) +
+      geom_text(nudge_y = 1.2, alpha = 0.8) +
+      geom_col(fill = "turquoise", alpha = 0.8) +
+      coord_cartesian(ylim = c(30, 70)) +
+      labs(x = "",
+           y = "Length of stay\n") +
+      theme_minimal() 
+    
   })
-
-  output$overview_deaths_plot <- renderPlot({
-
-    ggplotly(admissions_dt %>%
-               ggplot() +
-               aes(x = date, y = avg_admissions_by_week) +
-               geom_line(color = "steelblue") +
-               scale_x_date(name = "", limits = c(as.Date("2020-01-01", "%Y-%m-%d"), as.Date("2022-02-20", "%Y-%m-%d")), date_breaks = "3 months",
-                            date_minor_breaks = "1 month", date_labels = "%b %y") +
-               labs(y = "Average Admissions(weekly)") +
-               theme_minimal()
+  
+  ## WINTER INFO BOX
+  
+  infobox_predata_winter <- reactive(
+        
+    if (input$tabbox_id == "admissions"){
+      validate(
+        need(input$year_input, "")
+      )
+      d_mean <- mean((admissions_dt %>% filter(year %in% input$year_input, season == "Winter"))$avg_admissions_by_week) %>% round()
+      d_min <- min((admissions_dt %>% filter(year %in% input$year_input, season == "Winter"))$avg_admissions_by_week) %>% round()
+      d_max <- max((admissions_dt %>% filter(year %in% input$year_input, season == "Winter"))$avg_admissions_by_week) %>% round()
+      return(c(d_mean, d_min, d_max))
+    } else if (input$tabbox_id == "beds") {
+      validate(
+        need(input$year_input, "")
+      )
+      d_mean <- mean((beds_season_subset %>% filter(year %in% input$year_input, season == "Winter"))$avg_daily_beds_perc) %>% round()
+      d_min <- min((beds_season_subset %>% filter(year %in% input$year_input, season == "Winter"))$avg_daily_beds_perc) %>% round()
+      d_max <- max((beds_season_subset %>% filter(year %in% input$year_input, season == "Winter"))$avg_daily_beds_perc) %>% round()
+      return(c(d_mean, d_min, d_max))
+    } else if (input$tabbox_id == "los") {
+      validate(
+        need(input$year_input, "")
+      )
+      d_mean <- mean((los_season_subset %in% filter(year %in% input$year_input))$avg_los_day)
+      d_min <- min((los_season_subset %>% filter(year %in% input$year_input))$avg_los_days)
+      d_max <- max((los_season_subset %>% filter(year %in% input$year_input, season == "Winter"))$avg_los_days)
+      return(c(d_mean, d_min, d_max))
+    }
+  )
+  
+  output$infobox_winter_mean <- renderInfoBox(
+    infoBox("Mean Value (Winter)",
+            paste(infobox_predata_winter()[1]),
+            icon = icon("list"),
+            color = "red"
     )
-
-  })
-
-
+  )
+  
+  output$infobox_winter_min <- renderInfoBox(
+    infoBox("Mean Value (Winter)",
+            paste(infobox_predata_winter()[2]),
+            icon = icon("list"),
+            color = "red"
+    )
+  )
+  
+  output$infobox_winter_max <- renderInfoBox(
+    infoBox("Mean Value (Winter)",
+            paste(infobox_predata_winter()[3]),
+            icon = icon("list"),
+            color = "red"
+    )
+  )
+  
+  ## !WINTER INFO BOX
+  
+  infobox_predata_winter <- reactive(
+    
+    if (input$tabbox_id == "admissions"){
+      validate(
+        need(input$year_input, "")
+      )
+      d_mean <- mean((admissions_dt %>% filter(year %in% input$year_input, season != "Winter"))$avg_admissions_by_week) %>% round()
+      d_min <- min((admissions_dt %>% filter(year %in% input$year_input, season != "Winter"))$avg_admissions_by_week) %>% round()
+      d_max <- max((admissions_dt %>% filter(year %in% input$year_input, season != "Winter"))$avg_admissions_by_week) %>% round()
+      return(c(d_mean, d_min, d_max))
+    } else if (input$tabbox_id == "beds") {
+      validate(
+        need(input$year_input, "")
+      )
+      d_mean <- mean((beds_season_subset %>% filter(year %in% input$year_input, season != "Winter"))$avg_daily_beds_perc) %>% round()
+      d_min <- min((beds_season_subset %>% filter(year %in% input$year_input, season != "Winter"))$avg_daily_beds_perc) %>% round()
+      d_max <- max((beds_season_subset %>% filter(year %in% input$year_input, season != "Winter"))$avg_daily_beds_perc) %>% round()
+      return(c(d_mean, d_min, d_max))
+    } else if (input$tabbox_id == "los") {
+      validate(
+        need(input$year_input, "")
+      )
+      d_mean <- mean((los_season_subset %in% filter(year %in% input$year_input))$avg_los_day)
+      d_min <- min((los_season_subset %>% filter(year %in% input$year_input))$avg_los_days)
+      d_max <- max((los_season_subset %>% filter(year %in% input$year_input, season!= "Winter"))$avg_los_days)
+      return(c(d_mean, d_min, d_max))
+    }
+  )
+  
+  output$infobox_not_winter_mean <- renderInfoBox(
+    infoBox("Mean Value (other seasons)",
+            paste(infobox_predata_winter()[1]),
+            icon = icon("list"),
+            color = "blue"
+    )
+  )
+  
+  output$infobox_not_winter_min <- renderInfoBox(
+    infoBox("Mean Value (other seasons)",
+            paste(infobox_predata_winter()[2]),
+            icon = icon("list"),
+            color = "blue"
+    )
+  )
+  
+  output$infobox_not_winter_max <- renderInfoBox(
+    infoBox("Mean Value (other seasons)",
+            paste(infobox_predata_winter()[3]),
+            icon = icon("list"),
+            color = "blue"
+    )
+  )
+  
 }
--- dashboard_app/ui.R
+++ dashboard_app/ui.R
@@ -1,12 +1,12 @@
 
 ui <- dashboardPage(skin = "purple",
                     dashboardHeader(title = "Epic dashboard"),
-
+                    
                     # Sidebar content
                     dashboardSidebar(
-
+                      
                       sidebarMenu(
-
+                        
                         # lots of icons we can use here: https://fontawesome.com/icons/categories/medical-health
                         menuItem("Overview", tabName = "overview", icon = icon("book-medical"),
                                  badgeLabel = "temp", badgeColor = "green"),
@@ -18,95 +18,98 @@ ui <- dashboardPage(skin = "purple",
                                  badgeLabel = "temp", badgeColor = "red")
                       )
                     ),
-
+                    
                     # Body content
                     dashboardBody(
-
+                      
                       # First overview tab
                       tabItems(
-
-
+                        
+                        
                         # Overview tab content
                         tabItem(tabName = "overview",
                                 h2("Overview tab content"),
-
+                                
                                 fluidRow(
-                                  infoBoxOutput("info_box_mean", width = 4),
-                                  infoBoxOutput("info_box_max", width = 4),
-                                  infoBoxOutput("info_box_min", width = 4)
-
-                                ),
-
-                                fluidRow(
-                                  checkboxGroupInput(
-                                    "year_input", "Select Year" , c("2020", "2021"),
-                                    selected = c("2020", "2021"),
-                                    inline = T, 
+                                  box(width = 4,
+                                      checkboxGroupInput(
+                                        "year_input", "Select Year" , c("2020", "2021"),
+                                        selected = c("2020", "2021"),
+                                        inline = T, 
+                                      )
                                   )
                                 ),
-
-
-                                #Main visual (tabset with plots showing diff in
-                                #KPI's in Winter)
+                                
                                 fluidRow(
+                                  tabBox(width = 12,
+                                         id = "tabbox_id",
+                                         #Main visual (tabset with plots showing diff in
+                                         #KPI's in Winter)
+                                         
+                                         #Admissions
+                                         tabPanel("Admissions",
+                                                  value = "admissions",
+                                                  
+                                                  plotOutput("overview_admissions_plot")
+                                         ),
+                                         
+                                         #Bed capacity
+                                         tabPanel("Bed Capacity",
+                                                  value = "beds",
+                                                  
+                                                  plotOutput("overview_beds_plot")
+                                                  
+                                         ),
+                                         
+                                         #Length of stay
+                                         tabPanel("Length of stay",
+                                                  value = "los",
+                                                  
+                                                  plotOutput("overview_los_plot")
+                                                  
+                                         )
+                                  ) 
+                                ),  
+                                
+                                fluidRow(
+                                  box(width = 12,
+                                         
+                                         infoBoxOutput("infobox_winter_mean", width = 4),
+                                         infoBoxOutput("infobox_winter_max", width = 4),
+                                         infoBoxOutput("infobox_winter_min", width = 4)
+                                  )       
+                                ),  
+                                
+                                fluidRow(
+                                  box(width = 12,
 
-                                  tabsetPanel(
-
-                                    #Bed capacity
-                                    tabPanel("Bed Capacity",
-
-                                      plotOutput("overview_beds_plot")
-
-                                    ),
-
-                                    #Admissions
-                                    tabPanel("Admissions",
-
-                                      plotOutput("overview_admissions_plot")
-
-                                    ),
-
-                                    #Length of stay
-                                    tabPanel("Length of stay",
-
-                                      plotOutput("overview_los_plot")
-
-                                    ),
-
-                                    #
-                                    tabPanel("Deaths (from?)",
-
-                                      plotOutput("overview_deaths_plot")
-
-                                    )
-
+                                         infoBoxOutput("infobox_not_winter_mean", width = 4),
+                                         infoBoxOutput("infobox_not_winter_max", width = 4),
+                                         infoBoxOutput("infobox_not_winter_min", width = 4)
                                   )
-
-
                                 )
-
-
+                                
                         ),
-
+                        
                         # Temporal tab content
                         tabItem(tabName = "temporal",
                                 h2("Temporal tab content"),
                                 # Boxes need to be put in a row (or column)
                                 fluidRow(
                                   box(plotOutput("plot1", height = 250)), # Placeholder stuff
-
+                                  
                                   box(
                                     title = "Controls",
                                     sliderInput("slider", "Number of observations:", 1, 100, 50)
                                   )
                                 )
                         ),
-
+                        
                         # Geographic tab content
                         tabItem(tabName = "geographic",
                                 h2("Geographic tab content")
                         ),
-
+                        
                         # Demographic tab content
                         tabItem(tabName = "demographic",
                                 h2("Demographic tab content")
